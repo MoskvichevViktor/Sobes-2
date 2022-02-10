b7aa3d298675b5a0f2d2d5386bf9a9aca4ae2eb5
MoskvichevViktor <beldetal@yandex.ru>
2022-02-10 11:29:50 +0300

    finish

---
+++ src/main/java/lesson_6/HelloServlet.java
@@ -0,0 +1,27 @@
+package lesson_6;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello World!
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h2>" + message + "</h2>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
